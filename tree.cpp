646fa0ee249937cd20fa5b36553dcd5211094bf8
U-BSD\476724 <476724@SHS-10D9C064E.beaverton.k12.or.us>
2019-04-09 09:13:01 -0700

    Added some bad balancing, need to fix the self referencing, somewhere two or three nodes are pointing to themselves in a loop causing the print function to bug out

--- tree.cpp
+++ tree.cpp
@@ -8,6 +8,31 @@ Tree::Tree(){
 	blackheight = 0;
 	height = 0;
 }
+void Tree::shift(Node* node){
+	Node* parent = node->parent;
+	Node* opposite = 0;
+	if(parent->left == node){
+		opposite = parent->right;
+		parent->left = opposite;
+		parent->right = node;
+	}else{
+		opposite = parent->left;
+		parent->right = opposite;
+		parent->right = node;
+	}
+}
+Tree::Node* Tree::getuncle(Node* node){
+	if(node->parent != 0 && node->parent->parent != 0){
+		if (node->parent->parent->left == node->parent){
+			return node->parent->parent->right;
+		}else{
+			return node->parent->parent->left;
+		}
+	}
+	return 0;
+}
+
+
 void Tree::print(Node* root, int current_depth){
 	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 	if(root->right != 0)
@@ -27,15 +52,8 @@ void Tree::print(Node* root, int current_depth){
 
 void Tree::colorchange(Node* leaf){
 	Node* parent = leaf->parent;
-	Node* uncle = 0;
+	Node* uncle = getuncle(leaf);
 	
-	if(parent != 0 && parent->parent != 0){
-		if (parent->parent->left == parent){
-			uncle = parent->parent->right;
-		}else{
-			uncle = parent->parent->left;
-		}
-	}
 	if (parent != 0){
 		if (leaf->red == parent->red){
 			if(parent != 0)
@@ -77,12 +95,23 @@ void Tree::insert(int data){
 				newnode->parent = current;
 				current->right = newnode;
 				size++;
-				
+
+				if(newnode->parent->red == newnode->red){
+					if(newnode->parent->left == newnode && newnode->parent->right == 0){
+						Node* grandparent = newnode->parent->parent;
+						Node* parent = newnode->parent;
+
+						if(grandparent->parent != 0)
+							parent->parent = grandparent->parent;
+						else
+							parent->parent = 0;
+						
+						parent->right = grandparent;
+					}
+				}
 				if(newnode->parent->red == -1)
 					colorchange(newnode);
 				root->red = 1;
-
-				
 
 				break;
 			}
@@ -97,6 +126,20 @@ void Tree::insert(int data){
 				current->left = newnode;
 				size++;
 				
+				if(newnode->parent->red == newnode->red){
+					if(newnode->parent->left == newnode && newnode->parent->right == 0){
+						Node* grandparent = newnode->parent->parent;
+						Node* parent = newnode->parent;
+
+						parent->right = grandparent;
+						parent->parent = grandparent->parent;
+						if(grandparent->parent != 0 && grandparent->parent->left == grandparent)
+							grandparent->parent->left = parent;
+						else if(grandparent->parent != 0)
+							grandparent->parent->right = parent;
+						grandparent->parent = parent;
+					}
+				}
 				if(newnode->parent->red == -1)
 					colorchange(newnode);
 				root->red = 1;
--- tree.h
+++ tree.h
@@ -7,11 +7,16 @@ class Tree {
 			int red;
 			int data;
 		};
+
 		int blackheight, height, size;
 
 		Node* search(int);
 		void balance(Node* root);
 		void colorchange(Node*);
+		Node* getuncle(Node* node);
+
+		void rotate(Node*);
+		void shift(Node*);
 	public:
 		Tree();
 		Node* root;
